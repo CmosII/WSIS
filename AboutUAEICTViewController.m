55097ed85993b115f00e79d409de843fc9d0a556
CmosII <codiadmin@CoDITLiM04.local>
2014-02-16 15:23:39 +0400

    Added iPad
    Sidebar works for iPad (no horizontal)

--- AboutUAEICTViewController.m
+++ AboutUAEICTViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "AboutUAEICTViewController.h"
+#import "AppDelegate.h"
+#import "ECSlidingViewController.h"
+#import "MenuViewController.h"
 
 @interface AboutUAEICTViewController ()
+{
+    AppDelegate *appDelegate;
+}
 
 @end
 
@@ -27,6 +33,17 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    appDelegate = (AppDelegate*)[[UIApplication sharedApplication]delegate];
+    
+    self.view.layer.shadowOpacity = 0.75f;
+    self.view.layer.shadowRadius = 0.75f;
+    self.view.layer.shadowColor = [UIColor blackColor].CGColor;
+    
+    if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
+        self.slidingViewController.underLeftViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
+    }
+    
+    [self.view addGestureRecognizer:self.slidingViewController.panGesture];
 }
 
 - (void)didReceiveMemoryWarning
